42e2226cd865023133927723ab90ed773113de8d
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2024-03-06 11:16:31 -0300

    Start frontend project

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+function Home() {
+  return (
+    <main className='main-home'>
+      <div className='home-table-container'></div>
+
+    </main>
+  )
+}
+
+export default Home;
